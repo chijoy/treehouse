a0fef7811753a95adf1ac534783470468eab13fa
Joy McCaffrey <joymccaffreychicago@gmail.com>
2016-07-16 10:51:36 -0500

    Initialize with name, add transaction method.

--- bank_account.rb
+++ bank_account.rb
@@ -6,9 +6,11 @@ class BankAccount
 	end
 
 	def add_transaction(description, amount)
-		@transactions.push(description: )
+		@transactions.push(description: description, amount: amount)
 	end
 end
 
 bank_account = BankAccount.new("Joy")
+puts bank_account.inspect
+bank_account.add_transaction("Groceries", 40)
 puts bank_account.inspect
