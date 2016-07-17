ad921d69fa34027d8a02cddff0f016a87348128f
Joy McCaffrey <joymccaffreychicago@gmail.com>
2016-07-17 11:15:12 -0500

    Add full name and last name, first name method.

--- address_book/contact.rb
+++ address_book/contact.rb
@@ -23,6 +23,16 @@ class Contact
 		full_name += last_name
 		full_name
 	end
+
+	def last_first
+		last_first = last_name + ", " + first_name
+		if !middle_name.nil?
+			last_first += " "
+			last_first += middle_name.slice(0, 1)
+			last_first += "."
+		end
+		last_first
+	end
 end
 
 joy = Contact.new
@@ -30,6 +40,7 @@ joy.first_name = "Joy"
 joy.middle_name = "Anne"
 joy.last_name = "McCaffrey"
 puts joy.full_name
+puts joy.last_first
 
 stacey = Contact.new
 stacey.first_name = "Stacey"
