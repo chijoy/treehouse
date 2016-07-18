4205eab28e02888334ada29e527f4c01840db635
Joy McCaffrey <joymccaffreychicago@gmail.com>
2016-07-18 12:21:30 -0500

    Add method for print_results that was duplicated in find by name and find by number methods.

--- address_book/address_book.rb
+++ address_book/address_book.rb
@@ -16,13 +16,21 @@ class AddressBook
 				results.push(contact)
 			end
 		end
-		puts "Search results by name for (#{search})"
-		results.each do |contact|
-			puts contact.to_s("full_name")
-			contact.print_phone_numbers
-			contact.print_addresses
-			puts "\n"
+		print_results("Search results by name for (#{search})", results)
+	end
+
+	def find_by_phone_number(number)
+		results = []
+		search = number.gsub("-", "")
+		contacts.each do |contact|
+			contact.phone_numbers.each do |phone_number|
+				if phone_number.number.gsub("-", "").include?(search)
+					results.push(contact)
+				end
+			end
 		end
+		print_results("Search results by number for (#{search})", results)
+		
 	end
 
 	def print_contact_list
@@ -30,6 +38,16 @@ class AddressBook
 		puts "-" * 22
 		contacts.each do |contact|
 			puts contact.to_s("last_first")
+		end
+	end
+
+	def print_results(search, results)
+		puts search
+		results.each do |contact|
+			puts contact.to_s("full_name")
+			contact.print_phone_numbers
+			contact.print_addresses
+			puts "\n"
 		end
 	end
 end
@@ -49,6 +67,10 @@ address_book.print_contact_list
 puts
 puts
 address_book.find_by_name("joy")
+
+puts
+puts
+address_book.find_by_phone_number("313")
 
 
 
