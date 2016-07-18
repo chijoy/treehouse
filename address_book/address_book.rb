b28c6764b1b58dd02826a992d90b547c75ea9adb
Joy McCaffrey <joymccaffreychicago@gmail.com>
2016-07-18 14:21:44 -0500

    Add search function.

--- address_book/address_book.rb
+++ address_book/address_book.rb
@@ -12,6 +12,7 @@ class AddressBook
 			puts "Address Book"
 			puts "a: Add contact"
 			puts "p: Print Address Book"
+			puts "s: Search"
 			puts "e: Exit"
 			print "Enter your choice: "
 			input = gets.chomp.downcase
@@ -20,6 +21,12 @@ class AddressBook
 				add_contact
 			when "p"
 				print_contact_list
+			when "s"
+				print "Search term: "
+				search = gets.chomp
+				find_by_name(search)
+				find_by_address(search)
+				find_by_phone_number(search)
 			when "e"
 				break
 			end
