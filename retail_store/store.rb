64ee040a262b19c7e4f28b22e41ec749db5233b7
Joy McCaffrey <joymccaffreychicago@gmail.com>
2016-07-29 22:28:12 -0500

    Add in stock report.

--- retail_store/store.rb
+++ retail_store/store.rb
@@ -1,11 +1,53 @@
 module Inventoryable
 
+	def self.included(klass)
+		klass.extend(ClassMethods)
+		klass.extend(Enumerable)
+	end
+
+	module ClassMethods
+		def create(attributes)
+			object = new(attributes)
+			instances.push(object)
+			return object
+		end
+
+		def instances
+			@instances ||= []
+		end
+
+		def each(&block)
+			instances.each(&block)
+		end
+
+		def in_stock_report
+			puts "(self.to_s) In Stock Report"
+			reportable = instances.select{|instance| instance.in_stock? }
+			reportable.each do |item|
+				line = []
+				line.push("Item: #{item.atrributes[:name]}")
+				line.push("Stock: #{item.stock_count}")
+				if item.attributes.include?(:size)
+					line.push("Size: #{item.attributes[:size]}")
+				end
+				puts line.join("\t")
+			end
+			puts "\n"
+		end
+		
+	end
+
 	def stock_count
 		@stock_count ||= 0
 	end
 
 	def stock_count=(number)
 		@stock_count = number
+	end
+
+	def in_stock?
+		stock_count > 0
+	end
 end
 
 class Shirt
@@ -36,3 +78,10 @@ end
 shirt1 = Shirt.new(name: "MTF", size: "L")
 shirt2 = Shirt.new(nme: "MTF", size: "M")
 
+shirt1.stock_count = 10
+
+puts "Shirt 1 stock count: %s" % shirt1.stock_count
+puts "Shirt 2 stock count: %s" % shirt2.stock_count
+
+puts "Shirt 1 in stock?: %s" % shirt1.in_stock?
+puts "Shirt 2 in stock?: %s" % shirt2.in_stock?
