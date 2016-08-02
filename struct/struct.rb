cb2e21297e69dddb763dcb2281ac113f6b5e8cfe
Joy McCaffrey <joymccaffreychicago@gmail.com>
2016-08-02 18:20:17 -0500

    Demonstrate adding a block with Struct.

--- struct/struct.rb
+++ struct/struct.rb
@@ -11,9 +11,23 @@
 # puts customer.inspect
 
 # the same using struct
-Customer = Struct.new(:name, :email)
+# Customer = Struct.new(:name, :email)
+
+# customer = Customer.new("Joy", "joy@gmail.com")
+# puts customer.inspect
+
+# add a block
+Customer = Struct.new(:name, :email) do
+	def name_with_email
+		"#{name} at #{email}."
+	end
+end
+
 
 customer = Customer.new("Joy", "joy@gmail.com")
 puts customer.inspect
+puts customer.name_with_email
+customer.each do |attribute|
+	puts attribute
+end
 
-# add a block
