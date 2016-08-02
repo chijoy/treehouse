68853631f49eca5787b59bcf5f6ad8bbed444f2b
Joy McCaffrey <joymccaffreychicago@gmail.com>
2016-08-02 18:14:52 -0500

    Demonstrate Struct same as Class.

--- struct/struct.rb
+++ struct/struct.rb
@@ -1,10 +1,19 @@
-class Customer
-	attr_accessor :name, :email
+# Using a class
+# class Customer
+# 	attr_accessor :name, :email
 
-	def initialize(name, email)
-		@name, @email = name, email
-	end
-end
+# 	def initialize(name, email)
+# 		@name, @email = name, email
+# 	end
+# end
+
+# customer = Customer.new("Joy", "joy@gmail.com")
+# puts customer.inspect
+
+# the same using struct
+Customer = Struct.new(:name, :email)
 
 customer = Customer.new("Joy", "joy@gmail.com")
 puts customer.inspect
+
+# add a block
