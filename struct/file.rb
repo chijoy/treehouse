e89cff050544cf0b7889f3f761a024547cb34a78
Joy McCaffrey <joymccaffreychicago@gmail.com>
2016-08-03 14:46:23 -0500

    Document difference between w and a+.

--- struct/file.rb
+++ struct/file.rb
@@ -1,4 +1,16 @@
-File.open("example.txt", "w") do |file|
+# This will just write to the file, and will be overridden.
+# File.open("example.txt", "w") do |file|
+# 	print "Enter your name: "
+# 	name = gets.chomp
+# 	file.puts "Name: #{name}"
+
+# 	print "Enter your email: "
+# 	email = gets.chomp
+# 	file.puts "Email: #{email}"
+# end
+
+# This will add to the file.
+File.open("example.txt", "a+") do |file|
 	print "Enter your name: "
 	name = gets.chomp
 	file.puts "Name: #{name}"
